dc5f834ea7ab2033d2685f4e988298a8f6d57aab
akcarter86 <67606476+akcarter86@users.noreply.github.com>
2020-10-24 15:14:48 -0400

    updated the code to start my weather app

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,60 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-    alert ("Hello world!");
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="weather-app">
+          <h1>Methuen, MA</h1>
+
+          <ul>
+              <li>Clear</li>
+          </ul>
+
+            <div className="row">
+              <div className="col-6">
+                <img
+                    src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                    alt="Sunny"
+                />
+            <span className="temperature">
+              <span className="degrees">
+              {" "}
+              <strong>75</strong>
+              <a href="#" id="fahrenheit-link">
+                  °F
+              </a>
+              |
+              <a href="#" id="celsius-link">
+                  °C
+              </a>
+              </span>
+          </span>
+        </div>
+        <div className="col-6">
+          <ul>
+              <li>Precipitation: 5%</li>
+              <li>Humidity: 20%</li>
+              <li>Wind: 10mph</li>
+          </ul>
+        </div>
+      </div>
+    <span className="search-bar">
+        <form id="search-form">
+          <div className="form-group">
+              <input
+              type="search"
+              placeholder="Type the city..."
+              autoFocus="on"
+              autoComplete="off"
+              id="city-search"
+              />
+              <input type="button" value="Search" id="city" />
+          </div>
+        </form>
+    </span>
+      </div>
+    </div>
+</div>
+  )
 }
