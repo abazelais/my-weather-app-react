436b8a0a792de24a68ea643502e55627a4505d41
akcarter86 <67606476+akcarter86@users.noreply.github.com>
2020-11-02 22:55:12 -0500

    added GitHub link to project

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import Weather from "./Weather";
 
 export default function App() {
   return (
-    <Weather />
-  )
+    <div>
+      <Weather />
+
+      <p>
+        This project was coded by Amanda Carter and is
+        <a href="https://github.com/akcarter86/my-weather-app-react" target="_blank">open-sourced on GitHub</a>
+      </p>
+
+    </div>
+  );
 }
 
 
