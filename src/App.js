09e6dad86f35da3250808d9b4349135765e77022
akcarter86 <67606476+akcarter86@users.noreply.github.com>
2020-11-12 23:00:24 -0500

    removed extra items from App.css and updated App.js, also updated Weather.css and moved footer to App.js from Weather.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
+      <div className="Container">
       <Weather />
+      </div>
+      <footer>
+        <small>
+          This project was coded by Amanda Carter and it is {" "}
+          <a href="https://github.com/akcarter86/my-weather-app-react" 
+            target="_blank" 
+            rel="noopener noreferrer"> 
+            open-sourced on GitHub</a>
+        </small>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -20,74 +20,59 @@ export default function Weather() {
   let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(responseHandle);
   return (
-    <div className="App">
-      <div className="container">
-        <div className="weather-app">
-          
-          <form>
-            <div className="form-group">
-              <div className="row">
-                <div className="col-9">
-                  <input
-                  type="search"
-                  placeholder="Type the city..."
-                  autoFocus="on"
-                  autoComplete="off"
-                  className="form-control"
-                  />
-                </div>
-                <div className="col-3">
-                  <input type="submit" value="Search" className="btn btn-primary w-100" />
-                </div>
-              </div>
-            </div>
-          </form>
-          
-          <h1>Malden</h1>
-
-          <ul>
-            <li><CurrentDate /></li>
-            <li>Clear</li>
-          </ul>
-
-            <div className="row">
-              <div className="col-6">
-                <img
-                    src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                    alt="Sunny"
-                />
-                  <span className="temperature">
-                    <span className="degrees">
-                    {" "}
-                    <strong>75</strong>
-                  
-                        °F
-                    |
-                  
-                        °C
-                    
-                    </span>
-                </span>
-              </div>
-            <div className="col-6">
-              <ul>
-                  <li>Precipitation: 5%</li>
-                  <li>Humidity: 20%</li>
-                  <li>Wind: 10mph</li>
-              </ul>
-            </div>
+    <div className="weather-app">
+      
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+            type="search"
+            placeholder="Type the city..."
+            autoFocus="on"
+            autoComplete="off"
+            className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary w-100" />
           </div>
         </div>
+      </form>
+      
+      <h1>Malden</h1>
+
+      <ul>
+        <li><CurrentDate /></li>
+        <li>Clear</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
+          />
+            <span className="temperature">
+              <span className="degrees">
+              {" "}
+              <strong>75</strong>
+            
+                  °F
+              |
+            
+                  °C
+              
+              </span>
+          </span>
+        </div>
+        <div className="col-6">
+          <ul>
+              <li>Precipitation: 5%</li>
+              <li>Humidity: 20%</li>
+              <li>Wind: 10mph</li>
+          </ul>
+        </div>
       </div>
-        <p>
-          <small>
-        This project was coded by Amanda Carter and it is {" "}
-        <a href="https://github.com/akcarter86/my-weather-app-react" 
-          target="_blank" 
-          rel="noopener noreferrer"> 
-          open-sourced on GitHub</a>
-         </small>
-       </p>
     </div>
   );
 }
