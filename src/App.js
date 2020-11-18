aeefa034bdcc15377c5c1a504c27f1efe021afc9
akcarter86 <67606476+akcarter86@users.noreply.github.com>
2020-11-18 15:37:10 -0500

    Corrected spelling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="Container">
-      <Weather defaultcity="Boston"/>
+      <Weather defaultCity="Boston"/>
       </div>
       <footer>
         <small>
